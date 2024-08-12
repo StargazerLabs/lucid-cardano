cde5a24f399fd038b322de7f35621382407b311a
Christoph Waffler <waffler.christoph@gmail.com>
2024-08-12 23:20:24 +0200

    feat: publish new build

---
+++ dist/esm/package.d.ts
@@ -0,0 +1,10 @@
+declare namespace _default {
+    let name: string;
+    let version: string;
+    let license: string;
+    let author: string;
+    let description: string;
+    let repository: string;
+    let main: string;
+}
+export default _default;
